98cedc0afdb399289f9fcd4f584d7c34d461d2fe
Clara <almaclara2@gmail.com>
2020-10-14 19:37:28 +0200

    añadir prueba

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {  
-    $('.cabecera_nav li a').on('click',function(e){
+    $('ul li a').on('click',function(e){
         e.preventDefault();
         var strAncla=$(this).attr('href');
         $('body,html').stop(true,true).animate({
@@ -10,33 +10,33 @@ $(document).ready(function () {
     var elm = document.getElementById("logo");
     var check = "ಠ_ಠ Clara Albalá";
     var name = "xxxxxxxxxxxxxxx";
-    var array_search = ["A","a","B","b","c","D","d","E","e","F"," ","G","g","H","h","I","i","J",
+    var array_search = ["A","a","B","b","C","c","D","d","E","e","F"," ","G","g","H","h","I","i","J",
                         "j","K","k","L","l","ø","R","r","N","n","M","m","S","s","T","t"," ","x",
-                        "U","u","V","v"," ","Å","á","z","q","P","p","⨀","⋋","⊠","⊞","⊡","∎"];
+                        "U","u","V","v"," ","Å","á","z","q","P","p","⨀","⋋","⊠","⊞","⊡","∎","ಠ","_"];
+    
+    function setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
     function theLoop() {
-    setTimeout(function () {
-        
+    setTimeout(function () {  
         for (i = 0; i < 16; i++) {
-        if(name[i] == check[i]){
-            // it is correct
-        }else{
-            // it is false
-            var randnr = Math.floor(Math.random() * array_search.length);
-            console.log(array_search[randnr]);
-            name = setCharAt(name,i,array_search[randnr])
-        }
-        elm.innerHTML = name;
+            if(name[i] == check[i]){
+                // it is correct
+            }else{
+                // it is false
+                var randnr = Math.floor(Math.random() * array_search.length);
+                console.log(array_search[randnr]);
+                name = setCharAt(name,i,array_search[randnr])
+            }
+            elm.innerHTML = name;
         }
         if( name === check) {          // If i > 0, keep going
-            console.log('done')     // Call the loop again, and pass it the current value of i
+           console.log('done')     // Call the loop again, and pass it the current value of i
         }else{
             theLoop(); 
         }
     }, 100);
     };
     theLoop();
-    function setCharAt(str,index,chr) {
-        if(index > str.length-1) return str;
-        return str.substr(0,index) + chr + str.substr(index+1);
-    }
 });
